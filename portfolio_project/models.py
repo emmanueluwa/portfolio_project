a11538a3f052daeaa7b5ba721f4055b3cd2cecba
emmanueluwa <fulo1@gmail.com>
2021-06-22 15:26:52 +0100

    adding models and installing app

--- portfolio_project/models.py
+++ portfolio_project/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Portfolio(models.Model):
+    title = models.CharField(max_length=150)
+    description = models.CharField(max_length=150)
+    body = models.TextField()
+    link = models.CharField(max_length=100)
+    tag = models.ManyToManyField(Tag)
+    created_at = models.DateField(auto_now_add=True)
+
+
+
+    
+    
+
