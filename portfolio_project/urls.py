a4083c34e980ff4528f96b79179f9fc0fb84b7f0
emmanueluwa <fulo1@gmail.com>
2021-07-10 01:54:43 +0100

    adding tools detail to homepage, 1st section

--- portfolio_project/urls.py
+++ portfolio_project/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+from django.conf.urls import url
+from django.views.generic import RedirectView
+
 from . import views
 
 app_name = "portfolio_project"
@@ -10,6 +13,7 @@ urlpatterns = [
     path("projects", views.projects_page, name="projects"),
     path("post/<int:post_id>", views.post_page, name="post"),
 
+    path(r'^favicon\.ico$', RedirectView.as_view(url='/static/imges/favicon.ico'))
 ]
 
 urlpatterns += staticfiles_urlpatterns()
